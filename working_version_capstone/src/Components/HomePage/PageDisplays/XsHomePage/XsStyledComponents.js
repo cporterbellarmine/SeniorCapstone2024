feb0781ba6952e56f229ba450791804ee0ebdac7
Christina <cporter@bellarmine.edu>
2024-02-11 15:46:11 -0500

    git it

--- working_version_capstone/src/Components/HomePage/PageDisplays/XsHomePage/XsStyledComponents.js
+++ working_version_capstone/src/Components/HomePage/PageDisplays/XsHomePage/XsStyledComponents.js
@@ -1,7 +1,8 @@
+
 import styled from 'styled-components';
 import Accordion from 'react-bootstrap/Accordion';
 
-export const HomePageAccordionHeader = styled(Accordion.Header)`
+export const HomePageAccordionHeader = styled.h2`
     background-color: #000000;
     color: #ffffff;
     font-weight: bold;
