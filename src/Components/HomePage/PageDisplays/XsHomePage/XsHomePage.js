5ed7f376002c7316353ddcf0cfb720a82929388d
cporterbellarmine <cporter@bellarmine.edu>
2024-04-10 22:44:09 -0400

    styling home page

--- src/Components/HomePage/PageDisplays/XsHomePage/XsHomePage.js
+++ src/Components/HomePage/PageDisplays/XsHomePage/XsHomePage.js
@@ -10,7 +10,10 @@ import PuzzleGeneratorButtonRouting from '../NavButtons/PuzzleGenerator/PuzzleGe
 import ViewTopicsButtonRouting from '../NavButtons/ViewTopics/ViewTopicsButtonRouting';
 import FreeSearchLogo from '../../../Images/FreeSearchLogo.png';
 import { StyledContainer, StyledMenuCol } from '../HomePageComponentStyle';
+import { CreationColTitle, ExperienceColTitle, FutureColTitle, CreationColGradient, ExperienceColGradient, FutureColGradient, TitleText } from './XsStyledComponents';
 import './xs-css-id.css';
+
+/* Menu under title > Accordion > About */
 
 function HomePage() {
     return(
@@ -24,39 +27,70 @@ function HomePage() {
                 <Col>
                     <Row>
                         <h2 className='display-3'><u>Welcome to FreeSearch!</u></h2>
-        
-                        <b>Creation of FreeSearch</b>
-                         <p>
-                            FreeSearch was created in Spring 2024 by Christina Porter, a graduating Senior from Bellarmine University as a final capstone project.
-                            Word searches are a powerful and underutilized tool that can be used to enhance vocabulary and
-                            assess impact in cognitive function with persistent usage. FreeSearch is a free word-search generator that uses provided topics and associated words
-                            to generate a unique puzzle every render.
-                        </p>
-                        <br />
-
-                            <b>Prior Experience to FreeSearch</b>
-                        <p>
-                            FreeSearch is a result of Christina's 10 week period to design, learn, create, and implement a full-stack application. 
-                            Before FreeSearch, Christina had no experience with JavaScript, React, or MongoDB. 
-                        </p>
-                        <br />
-
-                        <b>References</b>
-                        <p>
-                            Several references were used in the creation of this project
-                            including official documentation, forums such as StackExchange, and tutorial sites such as W3Schools. A complete list can be found in the references page.
-                            Personal shout-out to Victor, Brinda, Tyrone, Persephone, and Shanna for their input on user scenarios and artwork.
-                        </p>
-                        <br />
-
+                    </Row>
+                    <Row>
+                        <CreationColTitle className='py-3 px-2 col-8'>
+                            <TitleText>
+                                <b>Creation of FreeSearch</b>
+                            </TitleText>
+                        </CreationColTitle>
+                        <CreationColGradient className='col-4'>
+                            <br />
+                        </CreationColGradient>
+                        <Col className='col-2'/>
+                    </Row>
+                    <Row className='py-2'>
+                        <Col className='col-9'>
+                            <p>
+                                FreeSearch was created in Spring 2024 by Christina Porter, a graduating Senior from Bellarmine University as a final capstone project.
+                                Word searches are a powerful and underutilized tool that can be used to enhance vocabulary and
+                                assess impact in cognitive function with persistent usage. FreeSearch is a free word-search generator that uses provided topics and associated words
+                                to generate a unique puzzle every render.
+                            </p>
+                        </Col>
+                        <Col className='col-3'/>
+                    </Row>
+                    <Row>
+                        <ExperienceColGradient className='col-4'>
+                            <br />
+                        </ExperienceColGradient>
+                        <ExperienceColTitle className='py-3 px-2 col-8 text-end'>
+                            <TitleText>
+                                <b>FreeSearch Learning Process</b>
+                            </TitleText>
+                        </ExperienceColTitle>
+                    </Row>
+                    <Row className='py-2'>
+                        <Col className='col-3'/>
+                        <Col className='col-9'>
+                            <p>
+                                There was 10 week period to design, create, and implement a full-stack application. 
+                                Before FreeSearch, Christina had no experience with JavaScript, React, or MongoDB. 
+                            </p>
+                        </Col>
                         
-                            <b>Future Implementation</b>
-                        <p>
-                            Upon further development, FreeSearch can be expanded to include functionality to edit, add, and remove topics and associated words. Eventually,
-                            Artificial Intelligence would become an asset to allow automatically generated word searches based off vocabulary central around the main topic
-                            of an article or other provided resource. Should FreeSearch opt to include interactive puzzle-solving solutions, results could be collected and
-                            analyzed to monitor cognitive capabilities over time.
-                        </p>
+                    </Row>
+                    <Row>
+                        <FutureColTitle className='py-3 px-2 col-8'>
+                            <TitleText>
+                                <b>Future Implementation</b>
+                            </TitleText>
+                        </FutureColTitle>
+                        <FutureColGradient className='col-4'>
+                            <br />
+                        </FutureColGradient>
+                    </Row>                
+                    <Row className='py-2'>
+                        <Col className='col-9'>
+                            <p>
+                                Upon further development, FreeSearch can be expanded to include functionality to edit, add, and remove topics and associated words. Eventually,
+                                Artificial Intelligence would become an asset to allow automatically generated word searches based off vocabulary central around the main topic
+                                of an article or other provided resource. Should FreeSearch opt to include interactive puzzle-solving solutions, results could be collected and
+                                analyzed to monitor cognitive capabilities over time.
+                            </p>
+                        </Col>
+                        <Col className='col-3'/>
+                        
                     </Row>
                     <Row>
                         <Accordion flush defaultActiveKey='0' className='accordion'>
--- src/Components/HomePage/PageDisplays/XsHomePage/XsStyledComponents.js
+++ src/Components/HomePage/PageDisplays/XsHomePage/XsStyledComponents.js
@@ -1,6 +1,41 @@
 import styled from 'styled-components';
 import Accordion from 'react-bootstrap/Accordion';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { colors } from '@mui/material';
 
 export const HomePageAccordionHeader = styled(Accordion.Header)`
     --bs-accordion-btn-bg: #000000;
+`;
+
+export const CreationColTitle = styled(Col)`
+    background-color: #a4036f;
+    color: #ffffff;
+`;
+
+export const CreationColGradient = styled(Col)`
+    background-image: linear-gradient(to right, #a4036f, #d9d9d9);
+`;
+
+export const ExperienceColTitle = styled(Col)`
+    background-color: #de4f3f;
+    color: #ffffff;
+`;
+
+export const ExperienceColGradient = styled(Col)`
+    background-image: linear-gradient(to left, #de4f3f, #d9d9d9);
+`;
+
+export const FutureColTitle = styled(Col)`
+    background-color: #19647e;
+    color: #ffffff;
+`;
+
+export const FutureColGradient = styled(Col)`
+    background-image: linear-gradient(to right, #19647e, #d9d9d9);
+`;
+
+export const TitleText = styled.h2`
+    -webkit-text-stroke-width: 1px;
+    -webkit-text-stroke-color: #000000;
 `;
